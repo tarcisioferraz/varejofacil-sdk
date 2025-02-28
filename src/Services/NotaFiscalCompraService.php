896e3b47e2105f8ccd635ccbc5858c561baa2334
Tarcísio <tarcisioruas@live.com>
2025-02-27 22:36:54 -0300

    Ajustando a classe NotaFiscalCompraService

--- src/Services/NotaFiscalCompraService.php
+++ src/Services/NotaFiscalCompraService.php
@@ -17,17 +17,23 @@ class NotaFiscalCompraService
         $this->sdk = $sdk;
     }
 
-    public function list(String $filter = '', $count = 500): Response
+    public function list(String $filter = '', $limit = null, $pace = 500): Response
     {
         if ($filter) {
             $filter = '&q=' . $filter;
         }
 
 
-        $resp = new Response(0, $count, 0);
+        $resp = new Response(0, $pace, 0);
+
+        $count = $pace;
+        if (!is_null($limit) && $limit < $pace)
+            $count = $limit;
+
 
         do {
-            $resposta = $this->sdk->get($this->resource . '?start=' . $resp->getStart() . $filter . '&count=' . $resp->getCount(), []);
+            $resposta = $this->sdk->get($this->resource . '?start=' . $resp->getStart() . $filter . '&count=' . $count, []);
+
             $resp->setTotal($resposta->total)
                 ->setCount($resposta->count)
                 ->moveStart($resposta->count);
@@ -39,34 +45,24 @@ class NotaFiscalCompraService
 
                     $notaFiscal->setId($item->id)
                         ->setLojaId($item->lojaId)
-                        ->setLocalId($item->localId)
-                        ->setOperacaoId($item->oeracaoId)
                         ->setFornecedorId($item->fornecedorId)
                         ->setDataEmissao($item->dataEmissao)
-                        ->setDataOperacao($item->dataOperacao)
-                        ->setDataAlteracao($item->dataAlteracao)
                         ->setNumeroNota($item->numeroNota)
                         ->setSerie($item->serie)
                         ->setChaveDaNfe($item->chaveDaNfe)
-                        ->setFuncionarioEmissorId($item->funcionarioEmissorId)
-                        ->setCfopId($item->cfopId)
                         ->setTipoDeOperacao($item->tipoDeOperacao)
                         ->setTipoDeFrete($item->tipoDeFrete)
                         ->setCondicaoDePagamento($item->condicaoDePagamento)
                         ->setProcessoDeEmissao($item->processoDeEmissao)
                         ->setTipoDeDocumentoFiscal($item->tipoDeDocumentoFiscal)
-                        ->setModalidade($item->modalidade)
                         ->setAtualizaCusto($item->atualizaCusto)
                         ->setAtualizaEstoque($item->atualizaEstoque)
-                        ->setGeraFiscal($item->geraFiscal)
-                        ->setCompoeABC($item->compoeABC)
                         ->setSituacao($item->situacao)
                         ->setObservacao($item->observacao)
                         ->setTipoDeGeracao($item->tipoDeGeracao)
                         ->setClassificacao($item->classificacao)
-                        ->setTipoDeGeracaoDeFinanceiro($item->tipoDeGeracaoDeFinanceiro)
                         ->setBaseDeCalculoDoICMS($item->baseDeCalculoDoICMS)
-                        ->setValordoICMS($item->valordoICMS)
+                        ->setValordoICMS($item->valorDoICMS)
                         ->setBaseDeCalculoDoICMSSubstituicaoTributaria($item->baseDeCalculoDoICMSSubstituicaoTributaria)
                         ->setValorDoIPI($item->valorDoIPI)
                         ->setValorDoFrete($item->valorDoFrete)
@@ -84,16 +80,40 @@ class NotaFiscalCompraService
                         ->setBaseDeCalculoFecopSubstituicaoTributaria($item->baseDeCalculoFecopSubstituicaoTributaria)
                         ->setValorFecopSubstituicaoTributaria($item->valorFecopSubstituicaoTributaria);
 
+                    if (isset($item->localId))
+                        $notaFiscal->setLocalId($item->localId);
+
+                    if (isset($item->funcionarioEmissorId))
+                        $notaFiscal->setFuncionarioEmissorId($item->funcionarioEmissorId);
+
+                    if (isset($item->operacaoId))
+                        $notaFiscal->setOperacaoId($item->operacaoId)
+                            ->setDataOperacao($item->dataOperacao);
+
+                    if (isset($item->cfopId))
+                        $notaFiscal->setCfopId($item->cfopId);
+
+                    if (isset($item->modalidade))
+                        $notaFiscal->setModalidade($item->modalidade);
+
+                    if (isset($item->geraFiscal))
+                        $notaFiscal->setGeraFiscal($item->geraFiscal);
+
+                    if (isset($item->compoeABC))
+                        $notaFiscal->setCompoeABC($item->compoeABC);
+
+                    if (isset($item->tipoDeGeracaoDeFinanceiro))
+                        $notaFiscal->setTipoDeGeracaoDeFinanceiro($item->tipoDeGeracaoDeFinanceiro);
+
 
                     foreach ($item->itens as $itemNota) {
                         $itemCompra = new ItemCompra();
 
-                        $itemCompra->setId($itemNota->Id)
+
+                        $itemCompra->setId($itemNota->id)
                             ->setProdutoId($itemNota->produtoId)
                             ->setSequencial($itemNota->sequencial)
                             ->setCompoeTotalDaNota($itemNota->compoeTotalDaNota)
-                            ->setCfopId($itemNota->cfopId)
-                            ->setUnidadeDeMedida($itemNota->unidadeDeMedida)
                             ->setQuantidadeDeItensNaUnidade($itemNota->quantidadeDeItensNaUnidade)
                             ->setQuantidade($itemNota->quantidade)
                             ->setQuantidadeCompleta($itemNota->quantidadeCompleta)
@@ -110,44 +130,25 @@ class NotaFiscalCompraService
                             ->setValorDoDAE($itemNota->valorDoDAE)
                             ->setPercentualDoDAE($itemNota->percentualDoDAE)
                             ->setValorTotalDoItem($itemNota->valorTotalDoItem)
-                            ->setSituacaoFiscalId($item->situacaoFiscalId)
                             ->setPercentualTributado($itemNota->percentualTributado)
-                            ->setCsosn($itemNota->csosn)
                             ->setCustoReposicao($itemNota->custoReposicao)
-                            ->setCustoFiscal($itemNota->custoFiscal)
                             ->setOutrasDespesasCompoeBaseDeCalculoIcms($itemNota->outrasDespesasCompoeBaseDeCalculoIcms)
-                            ->setSequencialItemPedido($itemNota->sequencialItemPedido)
                             ->setNcm($itemNota->ncm)
-                            ->setCest($itemNota->Cest)
+                            ->setCest($itemNota->cest)
                             ->setModalidadeDaBaseDeCalculo($itemNota->modalidadeDaBaseDeCalculo)
                             ->setPercentualICMSDeCompra($itemNota->percentualICMSDeCompra)
-                            ->setValorDoICMS($itemNota->valorICMS)
-                            ->setValorDoICMSNoSimples($itemNota->valorICMSNoSimples)
+                            ->setValorDoICMS($itemNota->valorDoICMS)
+                            ->setValorDoICMSNoSimples($itemNota->valorDoICMSNoSimples)
                             ->setBaseDeCalculoDoICMS($itemNota->baseDeCalculoDoICMS)
-                            ->setBaseDeCalculoDoICMSComSubstituicaoTributaria($itemNota->baseDeCalculoDoICMSSubstituicaoTributaria)
-                            ->setAliquotaDoICMSComSubstituicaoTributaria($itemNota->aliquotaDoICMSSubstituicaoTributaria)
-                            ->setValorDoICMSComSubstituicaoTributaria($itemNota->valorDoICMSSubstituicaoTributaria)
+                            ->setBaseDeCalculoDoICMSComSubstituicaoTributaria($itemNota->baseDeCalculoDoICMSComSubstituicaoTributaria)
+                            ->setAliquotaDoICMSComSubstituicaoTributaria($itemNota->aliquotaDoICMSComSubstituicaoTributaria)
+                            ->setValorDoICMSComSubstituicaoTributaria($itemNota->valorDoICMSComSubstituicaoTributaria)
                             ->setPercentualDeReducaoDASubstituicaoTributaria($itemNota->percentualDeReducaoDASubstituicaoTributaria)
                             ->setAliquotaDoICMS($itemNota->aliquotaDoICMS)
                             ->setAliquotaDoICMSDeVenda($itemNota->aliquotaDoICMSDeVenda)
                             ->setAliquotaDoICMSAntecipado($itemNota->aliquotaDoICMSAntecipado)
                             ->setValorDoICMSAntecipado($itemNota->valorDoICMSAntecipado)
                             ->setAliquotaNoSimples($itemNota->aliquotaNoSimples)
-                            ->setBaseDeCalculoDoIPI($itemNota->baseDeCalculoDoIPI)
-                            ->setAliquotaDoIPI($itemNota->aliquotaDoIPI)
-                            ->setTipoDeEntradaIPI($itemNota->tipoDeEntradaIPI)
-                            ->setValorDoIPI($itemNota->valorDoIPI)
-                            ->setCstDoPISId($itemNota->cstDoPISId)
-                            ->setBaseDeCalculoDoPIS($itemNota->baseDeCalculoDoPIS)
-                            ->setAliquotaDoPIS($itemNota->aliquotaDoPIS)
-                            ->setValorDoPIS($itemNota->valorDoPIS)
-                            ->setCstDoCOFINSId($itemNota->cstDoCOFINSId)
-                            ->setBaseDeCalculoDoCOFINS($itemNota->baseDeCalculoDoCOFINS)
-                            ->setAliquotaDoCOFINS($itemNota->aliquotaDoCOFINS)
-                            ->setValorDoCOFINS($itemNota->valorDoCOFINS)
-                            ->setBaseDeCalculoDoFecop($itemNota->baseDeCalculoDoFecop)
-                            ->setAliquotaDoFecop($itemNota->aliquotaDoFecop)
-                            ->setValorDoFecop($itemNota->valorDoFecop)
                             ->setBaseDeCalculoDoFecopSubstituto($itemNota->baseDeCalculoDoFecopSubstituto)
                             ->setAliquotaDoFecopSubstituto($itemNota->aliquotaDoFecopSubstituto)
                             ->setValorDoFecopSubstituto($itemNota->valorDoFecopSubstituto)
@@ -155,13 +156,63 @@ class NotaFiscalCompraService
                             ->setPercentualDiferimento($itemNota->percentualDiferimento)
                             ->setValorICMSDiferimento($itemNota->valorICMSDiferimento);
 
+                        if (isset($itemNota->baseDeCalculoDoIPI)) {
+                            $itemCompra->setBaseDeCalculoDoIPI($itemNota->baseDeCalculoDoIPI)
+                                ->setAliquotaDoIPI($itemNota->aliquotaDoIPI)
+                                ->setTipoDeEntradaIPI($itemNota->tipoDeEntradaIPI)
+                                ->setValorDoIPI($itemNota->valorDoIPI);
+                        }
+
+                        if (isset($itemNota->cfopId))
+                            $itemCompra->setCfopId($itemNota->cfopId);
+
+                        if (isset($itemNota->situacaoFiscalId))
+                            $itemCompra->setSituacaoFiscalId($itemNota->situacaoFiscalId);
+
+                        if (isset($itemNota->csosn))
+                            $itemCompra->setCsosn($itemNota->csosn);
+
+                        if (isset($itemNota->custoFiscal))
+                            $itemCompra->setCustoFiscal($itemNota->custoFiscal);
+
+                        if (isset($itemNota->cstDoPISId)) {
+                            $itemCompra->setCstDoPISId($itemNota->cstDoPISId)
+                                ->setBaseDeCalculoDoPIS($itemNota->baseDeCalculoDoPIS)
+                                ->setAliquotaDoPIS($itemNota->aliquotaDoPIS)
+                                ->setValorDoPIS($itemNota->valorDoPIS);
+                        }
+
+                        if (isset($itemNota->cstDoCOFINSId)) {
+                            $itemCompra->setCstDoCOFINSId($itemNota->cstDoCOFINSId)
+                                ->setBaseDeCalculoDoCOFINS($itemNota->baseDeCalculoDoCOFINS)
+                                ->setAliquotaDoCOFINS($itemNota->aliquotaDoCOFINS)
+                                ->setValorDoCOFINS($itemNota->valorDoCOFINS);
+                        }
+
+                        if (isset($itemNota->baseDeCalculoDoFecop)) {
+                            $itemCompra->setBaseDeCalculoDoFecop($itemNota->baseDeCalculoDoFecop)
+                                ->setAliquotaDoFecop($itemNota->aliquotaDoFecop)
+                                ->setValorDoFecop($itemNota->valorDoFecop);
+                        }
+
+                        if (isset($itemNota->sequencialItemPedido))
+                            $itemCompra->setSequencialItemPedido($itemNota->sequencialItemPedido);
+
+                        if (isset($itemNota->unidadeDeMedida))
+                            $itemCompra->setUnidadeDeMedida($itemNota->unidadeDeMedida);
+
+
                         $notaFiscal->addItem($itemCompra);
                     }
 
                     $resp->addItem($notaFiscal);
                 }
             }
-        } while ($resp->getStart() < $resp->getTotal());
+
+            $total = $resp->getTotal();
+            if (!is_null($limit) && $total > $limit)
+                $total = $limit;
+        } while ($resp->getStart() < $total);
 
         return $resp;
     }
